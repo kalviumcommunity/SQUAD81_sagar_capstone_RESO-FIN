1081c070cf12835d6b6122e14e751bbd250dc7c0
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-05-09 15:43:33 +0530

    schema update

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const adminSchema = mongoose.Schemma({
+const adminSchema = mongoose.Schema({
     username : {
         type : String,
         required : true,
